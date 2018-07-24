69c728fab5a7dee6cc2554e91554d0617ec47e83
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-23 20:41:03 -0500

    Add project files.

---
+++ VarAndDataTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VarAndDataTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            string Course = Console.ReadLine();
+            Console.WriteLine("What page number?");
+            string Page = Console.ReadLine();
+            int PageNumber = Convert.ToInt32(Page);
+            Console.WriteLine("Do you need help with anything? Please answer /"True / " or " / false / "");
+        }
+    }
+}
