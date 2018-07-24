a89d805935a2d420e5a7f3d6c930930d5bb4d52d
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-07-23 20:55:43 -0500

    Finish completing program

--- VarAndDataTypes/Program.cs
+++ VarAndDataTypes/Program.cs
@@ -13,7 +13,18 @@ namespace VarAndDataTypes
             Console.WriteLine("What page number?");
             string Page = Console.ReadLine();
             int PageNumber = Convert.ToInt32(Page);
-            Console.WriteLine("Do you need help with anything? Please answer /"True / " or " / false / "");
+            Console.WriteLine("Do you need help with anything? Please answer \"True\" or \"False\".");
+            string NeedHelp = Console.ReadLine();
+            bool RequestHelp = Convert.ToBoolean(NeedHelp);
+            Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
+            string Experiences = Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you'd like to provide?");
+            string Feedback = Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            string Hours = Console.ReadLine();
+            int StudyHours = Convert.ToInt32(Hours);
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
         }
     }
 }
